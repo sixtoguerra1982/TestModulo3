1b535499fd724ce5ed5a332030f77a3f34e11d80
= <=>
2023-05-04 18:16:54 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TestModulo3
---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const formbudget = document.getElementById("formbudget")
+formbudget.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    alert("Click")
+})
+
+const formexpenditure= document.getElementById("formexpenditure")
+formexpenditure.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    alert("Click formexpenditure")
+})
