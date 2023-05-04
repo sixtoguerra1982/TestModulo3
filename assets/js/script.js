b4c5220d5aa4b91571f465140c123c52356d8f11
= <=>
2023-05-04 19:49:24 -0400

    formularios ver 1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,27 @@
-const formbudget = document.getElementById("formbudget")
-formbudget.addEventListener("submit", (event) =>{
+// ... Retona numero con el siguiente formato → 123.456,789
+let formatNumber = (num) => {
+    return new Intl.NumberFormat('de-DE').format(num)
+}
+
+
+
+// Formulario de ingreso presupuesto
+const formBudget = document.getElementById("formbudget")
+formBudget.addEventListener("submit", (event) =>{
     event.preventDefault();
-    alert("Click")
+    let txtPresupuesto = document.getElementById("txtpresupuesto")
+    let inputBudgetValue = parseInt(document.getElementById("inputbudget").value)
+    if (inputBudgetValue) {
+        txtPresupuesto.innerHTML = formatNumber(inputBudgetValue)
+    } else {
+        alert("Ingrese Presupuesto")
+    }
 })
 
-const formexpenditure= document.getElementById("formexpenditure")
-formexpenditure.addEventListener("submit", (event) =>{
+
+// Formulario de ingreso de gastos
+const formExpenditure= document.getElementById("formexpenditure")
+formExpenditure.addEventListener("submit", (event) =>{
     event.preventDefault();
     alert("Click formexpenditure")
 })
